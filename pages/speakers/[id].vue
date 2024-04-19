ff0495c64a5f5b7f819317b55eb6e80937a1e77a
r20222 <101579892+r20222@users.noreply.github.com>
2024-04-19 18:37:07 +0200

    speakers data page

--- pages/speakers/[id].vue
+++ pages/speakers/[id].vue
@@ -1,12 +1,26 @@
 <template>
-    <p>{{ $route.params.id }}</p>
 <div v-for="dataObject in data">
+    <!-- $route.params.id hiermee haal je data uit de slug op -->
     <h1 v-if="dataObject.name === $route.params.id">{{ dataObject.name }}</h1>
+
+    <!-- deze code haalt alle data op die in het object staan waarbij de naam waarde overeenkomt met de waarde uit de slug -->
+    <div v-if="dataObject.name === $route.params.id">
+        <img :src="dataObject.avatar">
+        <!-- <p :v-for="datee in dataObject.edition.date">{{ datee }}</p> -->
+        <p>{{ dataObject.venue }}</p>
+        <p>{{ dataObject.talk.title }}</p>
+        <p>{{ dataObject.talk.description }}</p>
+
+      <div v-for="(value, key) in dataObject">
+        {{ key }}: {{ value }}
+      </div>
+    </div>
+
 </div>
-    <!-- <pre>{{ data[$route.params.id] }}</pre> -->
-    <pre>
+    <!-- <pre>{{ data }}</pre> -->
+    <!-- <pre>
     {{ carmenData }}
-  </pre>
+  </pre> -->
 </template>
 
 <script setup>
